a283f87b6e787a3ec5ece56afe3c88a6450a1ea9
Rochel Beer <rochelbeer26@gmail.com>
2023-03-27 21:19:52 -0400

    Add project files.

---
+++ MyFirstGitHub/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyFirstGitHub.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
